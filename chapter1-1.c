0f2ab53e9694b08f171b94996cc62c4896613493
dPwns3698 <dpwns8820@gmail.com>
2026-07-01 21:22:13 +0900

    Day 1 (7.1)

---
+++ chapter1-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Be happy!\n");
+    return 0;
+}
+    
---
+++ chapter1-2.c
@@ -0,0 +1,13 @@
+// c언어 기본 실행 예제, Be happy 출력. 파이썬 보다 복잡하다.
+// 같은 문자열 출력이여도 추가 적으로 작성해야할 코드가 많다.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+
+    printf("Be happy!");
+    system("pause");
+    return 0;
+}
